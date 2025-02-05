e8ee43f06ef7cd594b69d592f44ac00a9c0ea8bc
henryjgt <henry.j.g.thompson@outlook.com>
2025-02-05 11:03:59 +0000

    basic working implementation

--- main.py
+++ main.py
@@ -5,28 +5,25 @@ from GUI import Maze, Window
 
 WIDTH_PX: int = 700  # x
 HEIGHT_PX: int = 700  # y
+MARGIN: int = 50
+GRID_SIZE: int = 15  # no. cols & rows
 
 
 def main() -> None:
-    num_rows: int = 15
-    num_cols: int = 15
-    margin: int = 50
-    cell_size_x: int = m.floor((WIDTH_PX - 2 * margin) / num_cols)
-    cell_size_y: int = m.floor((HEIGHT_PX - 2 * margin) / num_rows)
-
     win = Window(WIDTH_PX, HEIGHT_PX)
+    cell_size_x: int = m.floor((WIDTH_PX - 2 * MARGIN) / GRID_SIZE)  # no. cols
+    cell_size_y: int = m.floor((HEIGHT_PX - 2 * MARGIN) / GRID_SIZE)  # no. rows
     maze = Maze(
-        margin,
-        margin,
-        num_rows,
-        num_cols,
+        MARGIN,
+        MARGIN,
+        GRID_SIZE,
+        GRID_SIZE,
         cell_size_x,
         cell_size_y,
         window=win,
         # seed=0,
     )
     maze.solve()
-
     win.wait_for_close()
 
 
